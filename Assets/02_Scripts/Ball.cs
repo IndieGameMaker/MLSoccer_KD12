2271c106f069833a0525938d0efcf7e4bbce60a9
Lee JaeHyun <myleje@gmail.com>
2023-05-30 13:32:18 +0900

    .

--- Assets/02_Scripts/Ball.cs
+++ Assets/02_Scripts/Ball.cs
@@ -12,12 +12,47 @@ public class Ball : MonoBehaviour
 
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter(Collision coll)
     {
+        if (coll.collider.CompareTag("RED_GOAL"))
+        {
+            // 블루팀 +1
+            players[0].AddReward(+1.0f);
+            // 레드팀 -1
+            players[1].AddReward(-1.0f);
 
+            // 볼 초기화(위치)
+            InitBall();
+            // 플레이어 학습 초기화
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+
+            ++blueScore;
+        }
+
+        if (coll.collider.CompareTag("BLUE_GOAL"))
+        {
+            // 블루팀 -1
+            players[0].AddReward(-1.0f);
+            // 레드팀 +1
+            players[1].AddReward(+1.0f);
+
+            // 볼 초기화(위치)
+            InitBall();
+            // 플레이어 학습 초기화
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+
+            ++redScore;
+        }
+    }
+
+    void InitBall()
+    {
+        rb.velocity = rb.angularVelocity = Vector3.zero;
+        transform.localPosition = new Vector3(0, 1, 0);
     }
 }
