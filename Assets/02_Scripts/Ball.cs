b4469214a0273957999390db87f7054ae9abadbf
Lee JaeHyun <myleje@gmail.com>
2023-05-30 13:21:19 +0900

    .

--- Assets/02_Scripts/Ball.cs
+++ Assets/02_Scripts/Ball.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.MLAgents;
 
 public class Ball : MonoBehaviour
 {
-
+    public Agent[] players;
 
     // Start is called before the first frame update
     void Start()
